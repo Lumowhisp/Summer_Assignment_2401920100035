4cad67990953bc8087d859442c9eca468cd13b7b
Lumowhisp <kumaraditya12981006@gmail.com>
2026-06-03 18:46:53 +0530

    Added Approach 2

--- Week_1/Day_3/squaring_sorted_array.cpp
+++ Week_1/Day_3/squaring_sorted_array.cpp
@@ -3,11 +3,33 @@ class Solution {
     //TC:O(NLOGN)
     //nlogn+n : nlogn dominates 
     //Approach 1:
+        // vector<int> sortedSquares(vector<int>& nums) {
+        //     for(int i=0;i<nums.size();i++){
+        //       nums[i]*=nums[i];
+        //     }
+        //     sort(nums.begin(),nums.end());
+        //     return nums;
+        // }
+
+    //Approach 2:
+    //Using Two Pointer :
+    
         vector<int> sortedSquares(vector<int>& nums) {
-            for(int i=0;i<nums.size();i++){
-              nums[i]*=nums[i];
+            int i=0;
+            int j=nums.size()-1;
+            int pos=nums.size()-1;
+            vector<int>ans(nums.size());
+            while(i<=j){
+                if(abs(nums[i])<=abs(nums[j])){
+                    ans[pos]=nums[j]*nums[j];
+                    j--;
+                }
+                else{
+                    ans[pos]=nums[i]*nums[i];
+                    i++;
+                }
+                pos--;
+            };
+            return ans;
             }
-            sort(nums.begin(),nums.end());
-            return nums;
-        }
     };
