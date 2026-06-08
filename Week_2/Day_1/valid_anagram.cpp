84e53a7307e1272a5e34ef9c464d43c77a9974a3
Lumowhisp <kumaraditya12981006@gmail.com>
2026-06-08 19:40:01 +0530

    Added : Day1 of Week1 Solution

---
+++ Week_2/Day_1/valid_anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            unordered_map<char,int>freq;
+            if(s.size()!=t.size()){
+                return false;
+            }
+            for(auto x:s){
+                freq[x]++;
+            }
+            for(auto ch:t){
+                freq[ch]--;
+            }
+            for(auto &it:freq){
+                if(it.second != 0){
+                    return false;
+                }
+            }
+            return true;
+    
+            
+        }
+    };
